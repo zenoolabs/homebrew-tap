462a00860c5bacbf35247c19bbea36ed9f2e30f6
Ceyhun Hallac <ceyhun@zenoo.com>
2025-02-17 23:30:25 +0100

    update click details

--- Formula/zenoo-cli.rb
+++ Formula/zenoo-cli.rb
@@ -10,8 +10,8 @@ class ZenooCli < Formula
   depends_on "python@3.9"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
-    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+    url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
+    sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
   end
 
   def install
