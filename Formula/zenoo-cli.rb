33dbbfdf9b09d2b0c2a5e281a2c7092b62875718
Ceyhun Hallac <ceyhun@zenoo.com>
2025-02-17 23:23:44 +0100

    install click

--- Formula/zenoo-cli.rb
+++ Formula/zenoo-cli.rb
@@ -9,6 +9,11 @@ class ZenooCli < Formula
 
   depends_on "python@3.9"
 
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
   def install
     virtualenv_install_with_resources
   end
